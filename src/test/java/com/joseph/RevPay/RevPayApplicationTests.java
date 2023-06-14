5ecd5a6b740e6efb001eb14fae1d69af9f886f97
jojo1234526 <121139382+jojo1234526@users.noreply.github.com>
2023-06-14 09:40:32 -0500

    Initial commit

---
+++ src/test/java/com/joseph/RevPay/RevPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseph.RevPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
