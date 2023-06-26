2de4a7b0b548b2e7b01884b6b37ad4563af375e7
jojo1234526 <121139382+jojo1234526@users.noreply.github.com>
2023-06-26 13:13:18 -0500

    test added

---
+++ src/test/java/com/joseph/RevPay/MyControllerTest.java
@@ -0,0 +1,41 @@
+package com.joseph.RevPay;
+
+import com.joseph.RevPay.Controller.UserController;
+import com.joseph.RevPay.Model.User;
+import com.joseph.RevPay.Service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(UserController.class)
+public class MyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testRegister() throws Exception {
+        User user = new User();
+        // Set up the user properties
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+        // etc...
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
